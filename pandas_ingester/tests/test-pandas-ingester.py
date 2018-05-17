6dbc6003128b0cadbcc1f0f145ededb6168e005b
Lawrence Crosby <crosbyla2012@gmail.com>
2018-05-16 19:10:34 -0500

    [WIP] add test stub

--- pandas_ingester/tests/test-pandas-ingester.py
+++ pandas_ingester/tests/test-pandas-ingester.py
@@ -1,9 +1,17 @@
 from pandas_ingester.pandas_ingester import main, load_data, save_pif
 
 import pandas as pd
+import os
 import pypif.pif as pif
 
 
 def test_load_data():
     df = load_data('../data/dsgdb9nsd_067017.xyz')
     assert isinstance(df, pd.DataFrame)
+
+
+def test_main():
+    data_file = '../data/dsgdb9nsd_067017.xyz'
+
+    main(data_file)
+    #assert os.path.isfile(os.path.join(os.getcwd(), 
