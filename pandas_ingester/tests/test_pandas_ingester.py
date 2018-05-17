f720a649cda0dfa157fbbee78bc678b73f70b96e
Lawrence Crosby <crosbyla2012@gmail.com>
2018-05-16 22:46:16 -0500

    fix path issue with tests

--- pandas_ingester/tests/test_pandas_ingester.py
+++ pandas_ingester/tests/test_pandas_ingester.py
@@ -2,16 +2,20 @@ from pandas_ingester.pandas_ingester import main, load_data, save_pif
 
 import pandas as pd
 import os
+from os.path import dirname, join, abspath
 import pypif.pif as pif
 
 
+base_dir = dirname(abspath(__file__)) + os.path.sep
+
 def test_load_data():
-    df = load_data('../data/dsgdb9nsd_067017.xyz')
+    data_file = join(base_dir, '..', 'data', 'dsgdb9nsd_067017.xyz')
+    df = load_data(data_file)
     assert isinstance(df, pd.DataFrame)
 
 
 def test_main():
-    data_file = '../data/dsgdb9nsd_067017.xyz'
+    data_file = join(base_dir, '..', 'data', 'dsgdb9nsd_067017.xyz')
 
     main(data_file)
     #assert os.path.isfile(os.path.join(os.getcwd(), 
