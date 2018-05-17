3d0d826d6b818fa24df73e8075b86b33e2aadfbe
Lawrence Crosby <crosbyla2012@gmail.com>
2018-05-17 10:26:39 -0500

    use list of data files to test and use re for file id

--- pandas_ingester/tests/test_pandas_ingester.py
+++ pandas_ingester/tests/test_pandas_ingester.py
@@ -1,21 +1,35 @@
 from pandas_ingester.pandas_ingester import main, load_data, save_pif
 
 import pandas as pd
+import re
 import os
 from os.path import dirname, join, abspath, isfile
 import pypif.pif as pif
 
-
+pat = re.compile(r'_(\d+).xyz')
 base_dir = dirname(abspath(__file__)) + os.path.sep
 
 def test_load_data():
-    data_file = join(base_dir, '..', 'data', 'dsgdb9nsd_067017.xyz')
-    df = load_data(data_file)
-    assert isinstance(df, pd.DataFrame)
+    data_dir = join(base_dir, '..', 'data')
+    data_files = [
+            'dsgdb9nsd_067017.xyz',
+            'dsgdb9nsd_017001.xyz',
+            'dsgdb9nsd_129001.xyz'
+            ]
+    for data_file in data_files:
+        df = load_data(join(data_dir, data_file))
+        assert isinstance(df, pd.DataFrame)
 
 
 def test_main():
-    data_file = join(base_dir, '..', 'data', 'dsgdb9nsd_067017.xyz')
-
-    main(data_file)
-    assert isfile(join(os.curdir,'{}.json'.format(67017)))
+    data_dir = join(base_dir, '..', 'data')
+    data_files = [
+            'dsgdb9nsd_067017.xyz',
+            'dsgdb9nsd_017001.xyz',
+            'dsgdb9nsd_129001.xyz'
+            ]
+    for data_file in data_files:
+        main(join(data_dir, data_file))
+        file_id = int(re.search(pat, data_file).group(1))
+        print(file_id)
+        assert isfile(join(os.curdir,'{}.json'.format(file_id)))
